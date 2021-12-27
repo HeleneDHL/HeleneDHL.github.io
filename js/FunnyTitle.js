127a210c272654b75370f5f943d5e78b6afe62cc
HeleneDHL <1977546563@qq.com>
2021-12-27 17:58:06 +0800

    Site updated: 2021-12-27 17:58:05

---
+++ js/FunnyTitle.js
@@ -0,0 +1,16 @@
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png");
+         document.title = '欢迎再次浏览！';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png");
+         document.title = '哈哈，您回来了！' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
